b89453ceba2b9b6764baf49e22b62c60f5c2e421
gooberkasper <22052177@kiit.ac.in>
2023-02-25 00:20:34 +0530

    initial commit

--- multiply.c
+++ multiply.c
@@ -1,5 +1,8 @@
 #include<stdio.h>
 
+int main2();
+float mul(float num1,float num2);
+
 int main()
 {
     int a, b, c;
@@ -7,15 +10,25 @@ int main()
     b=20;
     c=a*b;
     printf("The product of %d and %d is %d", a, b, c);
+    main2();
     return 0;   
 }
 
 int main2()
 {
     float a, b, c;
-    printf("Enter the numbers:");
-    scanf("%d %d", a ,b);
-    c=a*b;
-    printf("The product of %d and %d is %d", a, b, c);
+    printf("\nEnter the numbers:\n");
+    scanf("%f %f",&a,&b);
+//    c=a*b;
+//    printf("The product of %.2f and %.2f is %.2f", a, b, c);
+    c=mul(a,b);
+    printf("%.2f",c);
     return 0;
+}
+
+float mul(float num1,float num2)
+{
+    float c;
+    c=num1*num2;
+    return c;
 }
