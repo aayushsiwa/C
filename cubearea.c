39c7e722a7fcf1e3f1c3ee17c06cb4f21a0f0ac8
gooberkasper <22052177@kiit.ac.in>
2023-02-25 00:33:26 +0530

    initial commit

---
+++ cubearea.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int cube(int side);
+
+int cube(int side)
+{
+    int sa, csa;
+    sa=6*side*side;
+    csa=4*side*side;
+    printf("Surface area of cube is %d\n",sa);
+    printf("Curved surface are of cube is %d",csa);
+    return 0;
+}
+
+int main()
+{
+    int side;
+    printf("Enter the side of the cube:");
+    scanf("%d",&side);
+    cube(side);
+    return 0;
+}
