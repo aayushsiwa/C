a53fa40242fdb95c0fc3effd50dc933d3a36cb88
gooberkasper <22052177@kiit.ac.in>
2023-02-24 11:56:45 +0530

    initial commit

---
+++ Untitled-1.cpp
@@ -0,0 +1,174 @@
+#include <stdio.h>
+#include <math.h>
+#include <iostream>
+main()
+{
+    
+    int a, b;
+    a=3601;
+    b=a%60;
+    printf("%d",b);
+
+
+
+}
+
+
+
+
+
+/*#define pi 3.1416
+#define max 180
+main()
+{
+    int angle;
+    angle=0;
+    printf("           Angle      cos(angle)\n");
+    printf("--------------%f-------------\n",cos(0));
+    float x, y;
+    while (angle<=max)
+    {   
+        x=(pi/max)*angle;
+        y=cos(x);
+        printf("%d %f \n", angle, y);
+        angle=angle+10;
+    }
+}
+
+
+/*int multiply(int a, int b)
+{
+    int p;
+    p=a*b;
+    return(p);
+}
+main()
+{
+    int a=20;
+    int b=30;
+    int num3=multiply(a,b);
+    printf("The multiplication of %d and %d is %d",a , b , num3);
+}
+
+
+
+
+
+/*#define name "AAYUSH"
+#define hs "10,Subhash St."
+#define cp "Delhi,110075"
+main()
+{
+    printf("%s \n%s \n%s", name , hs ,cp);
+}
+
+
+
+/*#define Period 10
+#define Principal 5000.00
+main()
+{
+    int year;
+    float amount, value, interest;
+    amount=Principal;
+    interest=0.11;
+    year=0;
+    while(year<=Period)
+    {
+        printf("%2d %8.2f\n",year, amount);
+        value=amount+(interest*amount);
+        year=year+1;
+        amount=value;
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+/*main()
+{
+    printf("i see, i remember\n");
+    int number;
+    float amount;
+    number=100;
+    amount=30.75+20.15;
+    printf("%d\n",number);
+    printf("%5.23f",amount);
+}
+
+
+
+/*#include<iostream>
+#include<string.h>
+
+
+using namespace std;
+
+int main(){
+    int a;
+    for (int i = 0; i <= 6; ++i){
+        a=+i ;
+        cout<<a<<" "<<endl;
+    }
+    return 0;
+
+}
+
+
+
+/*int main(){
+    int a=5;
+    if(a>0)
+        cout<<"You are good";
+    else
+        cout<<"Try again";
+}
+
+
+
+/*int main(){
+    int n;
+    cin>>n;
+
+    if(n%2==0){
+        cout<<"The given number is an even number";
+    }
+    else{
+        cout<<"The given number is an odd number";
+    }
+}*/
+
+
+/*int main(){
+
+    
+    int mom;
+    cin>>mom;
+    
+    int dad;
+    cin>>dad;
+
+    int sum=mom+dad;
+
+    if (mom>dad) {
+        cout<<"Mom pays more\n";
+    }
+
+    else if (dad>mom){
+        cout<<"Dad pays more\n";
+    }
+    
+    else if (mom==dad){
+        cout<<"Both pay same\n";
+    }
+
+    cout<<"Total amount is:"<<sum;
+    return 0;
+}*/
