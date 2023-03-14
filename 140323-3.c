8207d2f4e95b307cd4ddcf15890bd0b937db3f76
gooberkasper <22052177@kiit.ac.in>
2023-03-14 08:50:13 +0530

    initial commit-factorial

---
+++ 140323-3.c
@@ -0,0 +1,15 @@
+//wap to calculate the factorial of a given number
+#include<stdio.h>
+int main()
+{
+    int i, f;
+    printf("Enter the number:\n");
+    scanf("%d",&i);
+    f=1;
+    for (i==i;i>0;i--)
+    {
+        f=f*i;
+    }
+    printf("The factorial of the given number is %d.",f);
+
+}
