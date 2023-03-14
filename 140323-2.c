9e4b9a9adc52964e1c7ac018e747acda72a446d2
gooberkasper <22052177@kiit.ac.in>
2023-03-14 08:35:16 +0530

    initial commit-for loop

---
+++ 140323-2.c
@@ -0,0 +1,12 @@
+//wap to print all the numbers in a given range
+#include<stdio.h>
+int main()
+{
+    int i,max;
+    printf("Enter the minimum and maximum numbers:\n");
+    scanf("%d %d",&i,&max);
+    for (i==i;i<=max;i++)
+    {
+        printf("%d\n",i);
+    }
+}
