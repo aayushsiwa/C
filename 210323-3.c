feda991e779bd23e8509e83f8c2f5e4a58a8c97a
gooberkasper <22052177@kiit.ac.in>
2023-03-21 09:11:43 +0530

    initial commit-series(1,1/3,1/5,...)

---
+++ 210323-3.c
@@ -0,0 +1,13 @@
+//wap to print sum of n terms of (1+1/3+1/5+...1/19)
+#include<stdio.h>
+int main()
+{
+    float i,sum=0,n;
+    printf("Enter the number of terms:\n");
+    scanf("%f",&n);
+    for (i=1;i<=n;i++)
+    {
+        sum+=1/(2*i-1);
+    }
+    printf("%.5f",sum);
+}
