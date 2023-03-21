798882fcf4a5ef36f7049ce0298b3c079476e1de
gooberkasper <22052177@kiit.ac.in>
2023-03-21 09:12:28 +0530

    initial commit-series(1/2,2/3,3/4,...)

---
+++ 210323-4.c
@@ -0,0 +1,13 @@
+//wap to print sum of n terms of (1/2+2/3+3/4+...+19/20)
+#include<stdio.h>
+int main()
+{
+    float a=0,i,sum=0,n;
+    printf("Enter the number of terms:\n");
+    scanf("%f",&n);
+    for (i=1;i<=n;i++)
+    {
+        sum+=((i)/(i+1));   
+    }
+    printf("%.5f",sum);
+}
