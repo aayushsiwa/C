38bc48658fafcf63942206c9cc6d8c79e60fc6c0
gooberkasper <22052177@kiit.ac.in>
2023-03-21 09:12:50 +0530

    initial commit-series(1*2,2*3,3*4,...)

---
+++ 210323-5.c
@@ -0,0 +1,13 @@
+//wap to print sum of n terms of (1*2+2*3+...+19*20)
+#include<stdio.h>
+int main()
+{
+    float i,sum=0,n;
+    printf("Enter the number of terms:\n");
+    scanf("%f",&n);
+    for (i=1;i<=n;i++)
+    {
+        sum+=(i)*(i+1);
+    }
+    printf("%f",sum);
+}
