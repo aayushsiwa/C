f9070440797a5628314b7f068a0283baede6b812
gooberkasper <22052177@kiit.ac.in>
2023-03-28 09:58:37 +0530

    initial commit-count -ve

---
+++ prac2.c
@@ -0,0 +1,18 @@
+//wap to input 15 numbers (both positive and negative) from user and print the 
+//count of negative numbers.
+#include<stdio.h>
+int main()
+{
+    int i,n,count=0;
+    printf("Enter 15 numbers both positive and negative:\n");
+    for (i=0;i<15;i++)
+    {
+        printf(":");
+        scanf("%d",&n);
+        if (n<0)
+        {
+            count+=1;
+        }
+    }
+    printf("There are %d negative numbers.",count);
+}
