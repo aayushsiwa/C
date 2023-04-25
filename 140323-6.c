19cc2a4d66d3e4a93919db56daddedb3ba904aa9
gooberkasper <22052177@kiit.ac.in>
2023-04-25 08:43:47 +0530

    added func-gcd

--- 140323-6.c
+++ 140323-6.c
@@ -1,9 +1,7 @@
 //wap to calculate the gcd of two number
 #include<stdio.h>
-int main()
+int gcd(int a, int b)
 {
-    int a,b;
-    scanf("%d %d",&a,&b);
     while(a!=b)
     {
         if (a>b)
@@ -16,4 +14,11 @@ int main()
         }
     }
     printf("%d",a);
+}
+int main()
+{
+    int a,b;
+    printf("Enter the numbers that you need to find gcd of:\n");
+    scanf("%d %d",&a,&b);
+    gcd(a,b);
 }
