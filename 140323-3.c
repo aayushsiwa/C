15914b6492e87d8c5b80745749ec18956e3c7434
gooberkasper <22052177@kiit.ac.in>
2023-04-25 08:30:43 +0530

    added func-fact

--- 140323-3.c
+++ 140323-3.c
@@ -1,15 +1,18 @@
 //wap to calculate the factorial of a given number
 #include<stdio.h>
-int main()
+int fact(int i)
 {
-    int i, f;
-    printf("Enter the number:\n");
-    scanf("%d",&i);
-    f=1;
+    int f=1;
     for (i==i;i>0;i--)
     {
         f=f*i;
     }
     printf("The factorial of the given number is %d.",f);
-
+}
+int main()
+{
+    int i;
+    printf("Enter the number:\n");
+    scanf("%d",&i);
+    fact(i);
 }
